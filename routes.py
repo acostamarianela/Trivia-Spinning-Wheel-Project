067f1516f69bf1401bf91d65105a7c54890a2833
Abril Zacarias <abrilzacarais6@gmail.com>
2023-09-18 11:13:56 -0300

    Co-authored-by: acostamarianela <acostamarianela@users.noreply.github.com>

--- routes.py
+++ routes.py
@@ -3,6 +3,10 @@ from flask import render_template, session, redirect, url_for, request
 from config import app
 
 @app.route('/')
+def pregunta():
+    return render_template('pregunta.html')
+
+@app.route('/ruleta')
 def ruleta():
     return render_template('ruleta.html')
 
