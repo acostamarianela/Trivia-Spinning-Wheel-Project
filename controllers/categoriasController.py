249cceaa5a4454734b11efe2a652b2d08d692031
hibren <canobren080603@gmail.com>
2023-09-19 10:14:25 -0300

    cambios en getpreguntas falta

--- controllers/categoriasController.py
+++ controllers/categoriasController.py
@@ -1,7 +1,7 @@
-#rom models.preguntasModel import PreguntasModel
+from models.preguntasModel import PreguntasModel
 from flask import flash, redirect, url_for
 import random
-from config import app, mysql
+import sqlite3
 
 class PreguntasController:
     def __init__(self):
@@ -17,18 +17,15 @@ class PreguntasController:
 }
         return preguntas_db
     
-    def consultaCategoria(self):
-        try:
-            # Utiliza la conexión MySQL en lugar de SQLite
-            cursor = mysql.connection.cursor()
-            # La sentencia SELECT se almacena en el cursor
-            cursor.execute("SELECT * FROM clientes")
-            # Devuelve todos los registros de la tabla clientes
-            result = cursor.fetchall()
-            print(result)
-            cursor.close()  # Cierra el cursor
-        except Exception as e:
-            print("Error executing SQL query:", e)
+    def consulta():
+        with sqlite3.connect("stock.db") as con:
+        cursor = con.cursor()
+        #la sentencia select se almacena en el cursor
+        cursor.execute("select * from clientes")         
+        #devuelve todos los registros del cursor(tabla clientes)
+        print(cursor.fetchall())
+
+#consulta()
 
     def getPreguntasPorCategoria(self, categoria):
         # Esta función podría consultar una base de datos u otro origen de datos
